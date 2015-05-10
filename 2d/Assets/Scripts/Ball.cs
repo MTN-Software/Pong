09630db2e707b8f2357df492f2905edda41a8287
OceanOSdev <ThomasTNF@live.com>
2015-05-10 14:20:14 -0500

    kinda works

---
+++ 2d/Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float ballVelocity = 3000;
+
+	Rigidbody rb;
+	bool isPlay;
+	int randInt;
+
+	// When Object is created
+	void Awake () 
+	{
+		rb = gameObject.GetComponent<Rigidbody> ();
+		randInt = Random.Range (1, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButton(0) == true && isPlay == false)
+		{
+			transform.parent = null;
+			isPlay = true;
+			rb.isKinematic = false;
+			switch (randInt) {
+			case 1:
+				rb.AddForce(new Vector3(ballVelocity, ballVelocity, 0));
+				break;
+			case 2:
+				rb.AddForce(new Vector3(-ballVelocity, -ballVelocity, 0));
+				break;
+			default:
+			break;
+			}
+		}
+	}
+}
---
+++ 2d/Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddleSpeed = 1F;
+	public Vector3 playerPos = new Vector3(0,0,0);			// player postision
+
+
+
+	// Use this for initialization
+	/*void Start () 
+	{
+		
+	}*/
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float yPos = gameObject.transform.position.y + (Input.GetAxis ("Vertical") * paddleSpeed);
+		playerPos = new Vector3 (-20, Mathf.Clamp (yPos, -11, 11), 0);
+		gameObject.transform.position = playerPos;
+	}
+}
